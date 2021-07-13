ca271ac729859f20fff32edc54f2e49b93e3ca89
VT-14 <GamingVT14@Gmail.com>
2021-07-13 14:36:38 -0400

    Register Altar Multiblocks
    Includes tweak in implementation for Ritual Multiblocks.

--- src/main/java/wayoftime/bloodmagic/compat/patchouli/RegisterPatchouliMultiblocks.java
+++ src/main/java/wayoftime/bloodmagic/compat/patchouli/RegisterPatchouliMultiblocks.java
@@ -5,16 +5,23 @@ import java.util.List;
 import java.util.Map;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
 import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import vazkii.patchouli.api.IMultiblock;
+import vazkii.patchouli.api.IStateMatcher;
 import vazkii.patchouli.api.PatchouliAPI;
 import vazkii.patchouli.api.PatchouliAPI.IPatchouliAPI;
 import wayoftime.bloodmagic.BloodMagic;
+import wayoftime.bloodmagic.altar.AltarComponent;
+import wayoftime.bloodmagic.altar.AltarTier;
+import wayoftime.bloodmagic.altar.ComponentType;
 import wayoftime.bloodmagic.common.block.BloodMagicBlocks;
+import wayoftime.bloodmagic.impl.BloodMagicAPI;
 import wayoftime.bloodmagic.ritual.EnumRuneType;
 import wayoftime.bloodmagic.ritual.Ritual;
 import wayoftime.bloodmagic.ritual.RitualComponent;
@@ -86,19 +93,36 @@ public class RegisterPatchouliMultiblocks
 						EnumRuneType rune = ritualMap.get(pos);
 						if (rune != null)
 						{
-							FindKey: for (EnumKeys key : EnumKeys.values())
+							String name = rune.name();
+							PickRune: switch (name)
 							{
-								if (rune == key.getRune())
-								{
-									row.append(key.getChar());
-									break FindKey;
-								}
+							case "BLANK":
+								row.append('B');
+								break PickRune;
+							case "WATER":
+								row.append('W');
+								break PickRune;
+							case "FIRE":
+								row.append('F');
+								break PickRune;
+							case "EARTH":
+								row.append('E');
+								break PickRune;
+							case "AIR":
+								row.append('A');
+								break PickRune;
+							case "DUSK":
+								row.append('D');
+								break PickRune;
+							case "DAWN":
+								row.append('d');
+								break PickRune;
 							}
 						} else
 						{
 							if (y == 0 && x == 0 && z == 0)
 							{
-								row.append('0'); // Center of Multiblock
+								row.append('0'); // Master Ritual Stone
 							} else
 							{
 								row.append('_'); // Patchouli's "Anything" Character.
@@ -112,48 +136,127 @@ public class RegisterPatchouliMultiblocks
 			patAPI.registerMultiblock(new ResourceLocation(BloodMagic.MODID, BloodMagic.RITUAL_MANAGER.getId(ritual)), multiblock);
 		}
 
-		// TODO: Blood Altars.
-	}
-
-	private enum EnumKeys
-	{
-		BLANK_RITUAL_STONE(EnumRuneType.BLANK, 'B', BloodMagicBlocks.BLANK_RITUAL_STONE.get()),
-		WATER_RITUAL_STONE(EnumRuneType.WATER, 'W', BloodMagicBlocks.WATER_RITUAL_STONE.get()),
-		FIRE_RITUAL_STONE(EnumRuneType.FIRE, 'F', BloodMagicBlocks.FIRE_RITUAL_STONE.get()),
-		EARTH_RITUAL_STONE(EnumRuneType.EARTH, 'E', BloodMagicBlocks.EARTH_RITUAL_STONE.get()),
-		AIR_RITUAL_STONE(EnumRuneType.AIR, 'A', BloodMagicBlocks.AIR_RITUAL_STONE.get()),
-		DUSK_RITUAL_STONE(EnumRuneType.DUSK, 'D', BloodMagicBlocks.DUSK_RITUAL_STONE.get()),
-		DAWN_RITUAL_STONE(EnumRuneType.DAWN, 'd', BloodMagicBlocks.DAWN_RITUAL_STONE.get()),
-		MASTER_RITUAL_STONE("ritual", BloodMagicBlocks.MASTER_RITUAL_STONE.get());
-
-		String type;
-		EnumRuneType rune;
-		Character character;
-		Block render;
-
-		EnumKeys(String type, Block block)
+		// Blood Altars
+		for (AltarTier tier : AltarTier.values())
 		{
-			this.type = type;
-			this.character = '0';
-			this.render = block;
-		}
+			String[][] pattern;
+			int shiftMultiblock = 1;
 
-		EnumKeys(EnumRuneType rune, Character character, Block block)
-		{
-			this.type = "ritual";
-			this.rune = rune;
-			this.character = character;
-			this.render = block;
-		}
+			if (tier.equals(AltarTier.ONE)) // Special Case Tier 1
+			{
+				pattern = new String[][] { { "0" }, { "_" } };
+				shiftMultiblock = 0;
+			} else if (tier.equals(AltarTier.TWO))
+			{ // Special Case Tier 2. Non-upgraded Runes.
+				pattern = new String[][] { { "___", "_0_", "___" }, { "rRr", "r_r", "rRr" } };
+			} else
+			{
+				Map<BlockPos, ComponentType> altarMap = Maps.newHashMap();
+				List<AltarComponent> components = tier.getAltarComponents();
+				int maxX = 0;
+				int minX = 0;
+				int maxY = 0;
+				int minY = 0;
+				int maxZ = 0;
+				int minZ = 0;
+				for (AltarComponent component : components) // Get Structure Dimensions.
+				{
+					BlockPos offset = component.getOffset();
+					altarMap.put(offset, component.getComponent());
+					int x = offset.getX();
+					int y = offset.getY();
+					int z = offset.getZ();
 
-		public EnumRuneType getRune()
-		{
-			return rune;
-		}
+					if (x > maxX)
+					{
+						maxX = x;
+					}
+					if (x < minX)
+					{
+						minX = x;
+					}
+					if (y > maxY)
+					{
+						maxY = y;
+					}
+					if (y < minY)
+					{
+						minY = y;
+					}
+					if (z > maxZ)
+					{
+						maxZ = z;
+					}
+					if (z < minZ)
+					{
+						minZ = z;
+					}
+				}
+				int xSize = 1 + maxX - minX;
+				int ySize = 1 + maxY - minY;
 
-		public char getChar()
-		{
-			return character;
+				pattern = new String[ySize][xSize];
+				for (int y = maxY; y >= minY; y--) // Top to Bottom
+				{
+					for (int x = minX; x <= maxX; x++) // West to East
+					{
+						StringBuilder row = new StringBuilder();
+						for (int z = minZ; z <= maxZ; z++) // North to South
+						{
+							BlockPos pos = new BlockPos(x, y, z);
+							ComponentType component = altarMap.get(pos);
+							if (component != null)
+							{
+								String name = component.name();
+								PickComponent: switch (name)
+								{
+								case "BLOODRUNE":
+									row.append('R');
+									break PickComponent;
+								case "NOTAIR":
+									row.append('P');
+									break PickComponent;
+								case "GLOWSTONE":
+									row.append('G');
+									break PickComponent;
+								case "BLOODSTONE":
+									row.append('S');
+									break PickComponent;
+								case "BEACON":
+									row.append('B');
+									break PickComponent;
+								case "CRYSTAL":
+									row.append('C');
+									break PickComponent;
+								}
+							} else
+							{
+								if (y == 0 && x == 0 && z == 0)
+								{
+									row.append('0'); // Blood Altar.
+								} else
+								{
+									row.append('_'); // Patchouli's "Anything" Character.
+								}
+							}
+						}
+						pattern[maxY - y][x - minX] = row.toString();
+					}
+				}
+			}
+
+			// TODO: try to make the "display" block cycle between usable blocks).
+
+			IStateMatcher bloodRuneSM = patAPI.predicateMatcher(BloodMagicBlocks.BLANK_RUNE.get(), state -> BloodMagicAPI.INSTANCE.getComponentStates(ComponentType.BLOODRUNE).contains(state));
+			IStateMatcher notAirSM = patAPI.predicateMatcher(Blocks.STONE_BRICKS, state -> state.getMaterial() != Material.AIR && !state.getMaterial().isLiquid());
+			IStateMatcher glowstoneSM = patAPI.predicateMatcher(Blocks.GLOWSTONE, state -> BloodMagicAPI.INSTANCE.getComponentStates(ComponentType.GLOWSTONE).contains(state));
+			IStateMatcher bloodStoneSM = patAPI.predicateMatcher(BloodMagicBlocks.BLOODSTONE.get(), state -> BloodMagicAPI.INSTANCE.getComponentStates(ComponentType.BLOODSTONE).contains(state));
+			IStateMatcher beaconSM = patAPI.predicateMatcher(Blocks.BEACON, state -> BloodMagicAPI.INSTANCE.getComponentStates(ComponentType.BEACON).contains(state));
+			IStateMatcher crystalSM = patAPI.predicateMatcher(Blocks.AIR, state -> BloodMagicAPI.INSTANCE.getComponentStates(ComponentType.CRYSTAL).contains(state));
+
+			IMultiblock multiblock = patAPI.makeMultiblock(pattern, '0', BloodMagicBlocks.BLOOD_ALTAR.get(), 'R', bloodRuneSM, 'P', notAirSM, 'G', glowstoneSM, 'S', bloodStoneSM, 'B', beaconSM, 'C', crystalSM, 'r', BloodMagicBlocks.BLANK_RUNE.get());
+			multiblock.offset(0, shiftMultiblock, 0);
+			patAPI.registerMultiblock(new ResourceLocation(BloodMagic.MODID, "altar_" + tier.name().toLowerCase()), multiblock);
 		}
 	}
 }
